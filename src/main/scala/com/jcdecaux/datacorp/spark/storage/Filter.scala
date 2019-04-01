b02bfa31092a824a538430945f2743cdc8274a6a
Marouane Felja <marouane.felja@jcdecaux.com>
2019-04-01 17:05:04 +0200

    making filter optional when value is not defined

--- src/main/scala/com/jcdecaux/datacorp/spark/storage/Filter.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/storage/Filter.scala
@@ -1,17 +1,18 @@
 package com.jcdecaux.datacorp.spark.storage
 
 /**
+  * Filter
   *
   * @param column   Column name
   * @param operator Equality and Relational Operators :
-  *                 =     equal to
   *                 =     equal to
   *                 !=    not equal to
   *                 >     greater than
   *                 >=    greater than or equal to
   *                 <     less than
   *                 <=    less than or equal to
-  * @param nature
+  * @param nature   datetime / date / string / long / int / double
   * @param value
   */
-case class Filter(column: String, operator: String, nature: String, value: String)
+case class Filter(column: String, operator: String, nature: String, value: Option[String]) {
+}
--- src/main/scala/com/jcdecaux/datacorp/spark/util/SqlExpressionUtils.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/util/SqlExpressionUtils.scala
@@ -13,9 +13,10 @@ object SqlExpressionUtils {
     * @return
     */
   def build(filters: Set[Filter]): String = {
-    filters.map{ filter =>
-      request(filter)
-    }.mkString(" AND ")
+    filters
+      .filter(row => row.value.isDefined)
+      .map(row => request(row))
+      .mkString(" AND ")
   }
 
   /**
@@ -27,16 +28,20 @@ object SqlExpressionUtils {
     * @return
     */
   def request(filter: Filter): String = {
-    filter.nature match {
-      case "datetime" =>
-        val time = if(filter.operator.contains(">")) "00:00:00" else "23:59:59"
-        s"${filter.column} ${filter.operator} cast('${filter.value} $time' as timestamp)"
-      case "date" =>
-        s"${filter.column} ${filter.operator} cast('${filter.value}' as date)"
-      case "string" =>
-        s"${filter.column} ${filter.operator} '${filter.value}'"
-      case _ =>
-        s"${filter.column} ${filter.operator} ${filter.value}"
+    if(filter.value.isDefined) {
+      filter.nature match {
+        case "datetime" =>
+          val time = if(filter.operator.contains(">")) "00:00:00" else "23:59:59"
+          s"${filter.column} ${filter.operator} cast('${filter.value.get} $time' as timestamp)"
+        case "date" =>
+          s"${filter.column} ${filter.operator} cast('${filter.value.get}' as date)"
+        case "string" =>
+          s"${filter.column} ${filter.operator} '${filter.value.get}'"
+        case _ =>
+          s"${filter.column} ${filter.operator} ${filter.value.get}"
+      }
+    } else {
+      ""
     }
   }
 }
