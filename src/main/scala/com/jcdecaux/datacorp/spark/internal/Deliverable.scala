88fe05cc383823e44503b02e4c825975b136b45e
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2019-06-26 17:21:25 +0200

    refactoring and add doc
    Signed-off-by: Xuzhou Qin <xuzhou.qin@jcdecaux.com>

--- src/main/scala/com/jcdecaux/datacorp/spark/internal/Deliverable.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/internal/Deliverable.scala
@@ -4,25 +4,55 @@ import com.jcdecaux.datacorp.spark.annotation.InterfaceStability
 
 import scala.reflect.runtime.{universe => ru}
 
+/**
+  * A deliverable is a container of an object that will be transferred by a [[DispatchManager]].
+  *
+  * @param payload data that will be transferred
+  * @param tag     type tag of the class T
+  * @tparam T type of the payload
+  */
 @InterfaceStability.Unstable
 class Deliverable[+T](val payload: T)(implicit tag: ru.TypeTag[T]) {
 
   var producer: Option[Class[_]] = None
+
+  /**
+    * Class of the consumer of this deliverable. When DispatchManager finds multiple dileverable with the same
+    * type, it will select the correct deliverable by looking at the consumer
+    */
   var consumer: Option[Class[_]] = None
 
   def tagInfo: ru.Type = tag.tpe
 
   def get: T = payload
 
+  /**
+    * Compare the type of two deliverable
+    *
+    * @param t a deliverable object
+    * @return
+    */
   def ==(t: Deliverable[_]): Boolean = this.tagInfo.equals(t.tagInfo)
 
-  def classInfo = payload.getClass
+  def classInfo: Class[_] = payload.getClass
 
+  /**
+    * Set producer of this deliverable
+    *
+    * @param t class of producer
+    * @return
+    */
   def setProducer(t: Class[_]): this.type = {
     producer = Some(t)
     this
   }
 
+  /**
+    * Set consumer of this deliverable
+    *
+    * @param t class of consumer
+    * @return
+    */
   def setConsumer(t: Class[_]): this.type = {
     consumer = Some(t)
     this
--- src/main/scala/com/jcdecaux/datacorp/spark/internal/DispatchManager.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/internal/DispatchManager.scala
@@ -1,18 +1,22 @@
 package com.jcdecaux.datacorp.spark.internal
 
-import com.jcdecaux.datacorp.spark.annotation.Delivery
+import com.jcdecaux.datacorp.spark.annotation.{Delivery, InterfaceStability}
 import com.jcdecaux.datacorp.spark.transformation.Factory
 
 import scala.collection.mutable.ArrayBuffer
-import scala.reflect.ClassTag
 import scala.reflect.runtime.{universe => ru}
 
 /**
   * DispatchManager will handle the data dispatch between different stages.
   *
-  * It will collect a [[Deliverable]] from a [[Factory]] and send the right deliverable to another factory
+  * It can:
+  * <ul>
+  * <li>collect a [[Deliverable]] from a [[Factory]]</li>
+  * <li>find the right [[Deliverable]] from its pool and send it to a [[Factory]]</li>
+  * </ul>
   */
-class DispatchManager extends Logging {
+@InterfaceStability.Evolving
+private[spark] class DispatchManager extends Logging {
 
   val deliveries: ArrayBuffer[Deliverable[_]] = ArrayBuffer()
 
@@ -45,36 +49,49 @@ class DispatchManager extends Logging {
     }
   }
 
-  def getDeliveries(deliveryType: ru.Type): Array[Deliverable[_]] = deliveries.filter(d => d.tagInfo == deliveryType).toArray
-
-  def collectDeliverable(factory: Factory[_]): this.type = {
-    setDelivery(factory.deliver())
-  }
+  /**
+    * Get all the [[Deliverable]] of the given type
+    *
+    * @param deliveryType type of data
+    * @return
+    */
+  def getDeliveries(deliveryType: ru.Type): Array[Deliverable[_]] =
+    deliveries.filter(d => d.tagInfo == deliveryType).toArray
 
   /**
-    * Dispatch the right deliverable object to the corresponding methods (denoted by the @Delivery annotation) of a factory
+    * Collect a [[Deliverable]] from a [[Factory]]
+    *
+    * @param factory a factory object
+    * @return
+    */
+  def collectDeliverable(factory: Factory[_]): this.type = setDelivery(factory.deliver())
+
+  /**
+    * Dispatch the right deliverable object to the corresponding methods
+    * (denoted by the @Delivery annotation) of a factory
     *
     * @param factory target factory
-    * @param tag     implicit runtime type tag
     * @tparam T Type of factory
     * @return
     */
-  def dispatch[T <: Factory[_]](factory: T)(implicit tag: ru.TypeTag[T], classTag: ClassTag[T]): this.type = {
+  def dispatch[T <: Factory[_]](factory: T): this.type = {
 
     getDeliveryAnnotatedMethod(factory)
       .foreach({
         methodName =>
+          // Loop through the type of all arguments of a method and get the Deliverable that correspond to the type
           val args = methodName._2.map({
             argsType =>
-              log.debug(s"Dispatch $argsType to ${tag.tpe}.${methodName._1}")
+              log.debug(s"Dispatch $argsType by calling ${factory.getClass.getCanonicalName}.${methodName._1}")
+
               getDelivery(argsType, factory.getClass) match {
                 case Some(thing) => thing
                 case _ => throw new NoSuchElementException(s"Can not find type $argsType from dispatch manager")
               }
           })
 
+          // Invoke the method with its name and arguments
           def method = factory.getClass.getMethod(methodName._1, args.map(_.classInfo): _*)
-
           method.invoke(factory, args.map(_.get.asInstanceOf[Object]): _*)
       })
 
@@ -84,31 +101,36 @@ class DispatchManager extends Logging {
   /**
     * Get the name and arguments type of methods having [[com.jcdecaux.datacorp.spark.annotation.Delivery]] annotation.
     *
-    * @param factory factory
-    * @param tag     implicit TypeTag
+    * @param obj an object
     * @tparam T type of factory
     * @return a Map of method name -> list of arguments type
     */
-  def getDeliveryAnnotatedMethod[T](factory: T)(implicit tag: ru.TypeTag[T], classTag: ClassTag[T]): Map[String, List[ru.Type]] = {
-    log.debug(s"Fetch methods of ${factory.getClass} having Delivery annotation")
+  private[this] def getDeliveryAnnotatedMethod[T](obj: T): Map[String, List[ru.Type]] = {
+    log.debug(s"Fetch methods of ${obj.getClass} having Delivery annotation")
 
-    val runtimeMirror = ru.runtimeMirror(getClass.getClassLoader)
-    val classSymbol = runtimeMirror.classSymbol(factory.getClass)
-
-    val methodsWithDeliveryAnnotation = classSymbol.info.decls
-      .filter(x => x.annotations.exists(y => y.tree.tpe =:= ru.typeOf[Delivery]))
+    // Black magic XD
+    val classSymbol = ru.runtimeMirror(getClass.getClassLoader).classSymbol(obj.getClass)
+    val methodsWithDeliveryAnnotation = classSymbol.info.decls.filter({
+      x => x.annotations.exists(y => y.tree.tpe =:= ru.typeOf[Delivery])
+    })
 
     if (methodsWithDeliveryAnnotation.isEmpty) log.info("No method having @Delivery annotation")
 
     methodsWithDeliveryAnnotation.map({
       mth =>
         if (mth.isMethod) {
-          log.debug(s"Find @Delivery annotated method ${factory.getClass.getCanonicalName}.${mth.name}")
+          log.debug(s"Find @Delivery annotated method ${obj.getClass.getCanonicalName}.${mth.name}")
           (mth.name.toString, mth.typeSignature.paramLists.head.map(_.typeSignature))
         } else {
-          log.debug(s"Find @Delivery annotated value ${factory.getClass.getCanonicalName}.${mth.name}")
-          val newMethod = classSymbol.info.decls.find(_.name.toString == mth.name.toString.trim + "_$eq").get
-          (newMethod.name.toString, newMethod.typeSignature.paramLists.head.map(_.typeSignature))
+          log.debug(s"Find @Delivery annotated value ${obj.getClass.getCanonicalName}.${mth.name}")
+
+          // val newMethod = classSymbol.info.decls.find(_.name.toString == mth.name.toString.trim + "_$eq").get
+          // (newMethod.name.toString, newMethod.typeSignature.paramLists.head.map(_.typeSignature))
+
+          /*
+           * If an annotated value was found, then return the default setter created by compiler, which is {valueName}_$eq.
+           */
+          (mth.name.toString.trim + "_$eq", List(mth.typeSignature))
         }
 
     }).toMap
