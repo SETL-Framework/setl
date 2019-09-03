0821aa1c0e3cf044d3fe30c9ef4c08e920a40da1
Qin Xuzhou <xuzhou.qin@jcdecaux.com>
2019-09-03 17:32:07 +0200

    refactor: add new class ConnectorConf to centralize configurations
    Signed-off-by: Xuzhou Qin <xuzhou.qin@jcdecaux.com>

--- src/main/scala/com/jcdecaux/datacorp/spark/storage/connector/ParquetConnector.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/storage/connector/ParquetConnector.scala
@@ -1,7 +1,7 @@
 package com.jcdecaux.datacorp.spark.storage.connector
 
 import com.jcdecaux.datacorp.spark.annotation.InterfaceStability
-import com.jcdecaux.datacorp.spark.config.Conf
+import com.jcdecaux.datacorp.spark.config.{Conf, ConnectorConf}
 import com.jcdecaux.datacorp.spark.enums.Storage
 import com.jcdecaux.datacorp.spark.util.TypesafeConfigUtils
 import com.typesafe.config.Config
@@ -12,9 +12,12 @@ import org.apache.spark.sql._
   */
 @InterfaceStability.Evolving
 class ParquetConnector(override val spark: SparkSession,
-                       override val options: Map[String, String]) extends FileConnector(spark, options) {
+                       override val options: ConnectorConf) extends FileConnector(spark, options) {
 
   override val storage: Storage = Storage.PARQUET
+  this.options.setStorage(storage)
+
+  def this(spark: SparkSession, options: Map[String, String]) = this(spark, ConnectorConf.fromMap(options))
 
   def this(spark: SparkSession, path: String, saveMode: SaveMode) =
     this(spark, Map[String, String](
@@ -26,41 +29,4 @@ class ParquetConnector(override val spark: SparkSession,
 
   def this(spark: SparkSession, conf: Conf) = this(spark = spark, options = conf.toMap)
 
-  /**
-    * Read a [[DataFrame]] from a parquet file with the path defined during the instantiation
-    *
-    * @return DataFrame
-    */
-  override def read(): DataFrame = {
-    log.debug(s"Reading csv file from ${absolutePath.toString}")
-    val df = reader.parquet(listFiles(): _*)
-    if (dropUserDefinedSuffix & df.columns.contains(userDefinedSuffix)) {
-      df.drop(userDefinedSuffix)
-    } else {
-      df
-    }
-  }
-
-  /**
-    * Write a [[DataFrame]] into parquet file
-    *
-    * @param df     dataframe to be written
-    * @param suffix optional, String, write the df in a sub-directory of the defined path
-    */
-  override def write(df: DataFrame, suffix: Option[String] = None): Unit = {
-    suffix match {
-      case Some(s) =>
-        checkPartitionValidity(true)
-        writeParquet(df, s"${this.absolutePath.toString}/$userDefinedSuffix=$s")
-      case _ =>
-        checkPartitionValidity(false)
-        writeParquet(df, absolutePath.toString)
-    }
-  }
-
-  private[this] def writeParquet(df: DataFrame, filepath: String): Unit = {
-    log.debug(s"Write DataFrame to $filepath")
-    initWriter(df)
-    writer.parquet(filepath)
-  }
 }
