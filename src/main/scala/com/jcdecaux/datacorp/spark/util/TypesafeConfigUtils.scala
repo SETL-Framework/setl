a98ee67231d7dba30e5bd9239b069a26f9afdc93
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2019-12-02 12:01:13 +0100

    chore: refactor TypeSafeConfigUtils with currying function
    Signed-off-by: Xuzhou Qin <xuzhou.qin@jcdecaux.com>

--- src/main/scala/com/jcdecaux/datacorp/spark/util/TypesafeConfigUtils.scala
+++ src/main/scala/com/jcdecaux/datacorp/spark/util/TypesafeConfigUtils.scala
@@ -6,109 +6,71 @@ import com.typesafe.config.{Config, ConfigException}
 object TypesafeConfigUtils {
 
   def getAs[T](config: Config, path: String)(implicit getter: ConfigGetter[T]): Option[T] = getter.get(config, path)
-  
-  def getMap(config: Config): Map[String, String] = {
-    import scala.collection.JavaConverters._
-    config.entrySet().asScala.map(x => x.getKey -> x.getValue.unwrapped().toString).toMap
+
+  private[this] def _get[T](path: String): (String => T) => Option[T] = (fun: String => T) => {
+    try {
+      Option(fun(path))
+    } catch {
+      case _: ConfigException.Missing => None
+      case e: ConfigException.WrongType => throw e
+    }
   }
 
   private[spark] implicit val stringGetter: ConfigGetter[String] = new ConfigGetter[String] {
     override def get(config: Config, path: String): Option[String] = {
-      try {
-        Option(config.getString(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[String](path)(config.getString)
     }
   }
 
   private[spark] implicit val intGetter: ConfigGetter[Int] = new ConfigGetter[Int] {
     override def get(config: Config, path: String): Option[Int] = {
-      try {
-        Option(config.getInt(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Int](path)(config.getInt)
     }
   }
 
   private[spark] implicit val longGetter: ConfigGetter[Long] = new ConfigGetter[Long] {
     override def get(config: Config, path: String): Option[Long] = {
-      try {
-        Option(config.getLong(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Long](path)(config.getLong)
     }
   }
 
   private[spark] implicit val floatGetter: ConfigGetter[Float] = new ConfigGetter[Float] {
     override def get(config: Config, path: String): Option[Float] = {
-      try {
-        Option(config.getLong(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Float](path)(x => config.getString(x).toFloat)
     }
   }
 
   private[spark] implicit val doubleGetter: ConfigGetter[Double] = new ConfigGetter[Double] {
     override def get(config: Config, path: String): Option[Double] = {
-      try {
-        Option(config.getLong(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Double](path)(config.getDouble)
     }
   }
 
   private[spark] implicit val booleanGetter: ConfigGetter[Boolean] = new ConfigGetter[Boolean] {
     override def get(config: Config, path: String): Option[Boolean] = {
-      try {
-        Option(config.getBoolean(path))
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Boolean](path)(config.getBoolean)
     }
   }
 
   private[spark] implicit val listGetter: ConfigGetter[Array[AnyRef]] = new ConfigGetter[Array[AnyRef]] {
     override def get(config: Config, path: String): Option[Array[AnyRef]] = {
-      try {
-        Option(config.getList(path).unwrapped().toArray())
-      } catch {
-        case _: ConfigException.Missing => None
-        case e: ConfigException.WrongType => throw e
-      }
+      _get[Array[AnyRef]](path)(x => config.getList(x).unwrapped().toArray())
     }
   }
 
   private[spark] implicit val StorageGetter: ConfigGetter[Storage] = new ConfigGetter[Storage] {
     override def get(config: Config, path: String): Option[Storage] = {
-
-      try {
-        Option(Storage.valueOf(config.getString(path)))
-      } catch {
-        case m: ConfigException.Missing => throw m
-        case w: ConfigException.WrongType => throw w
-      }
-
+      _get[Storage](path)(x => Storage.valueOf(config.getString(x)))
     }
   }
 
   def getList(config: Config, path: String): Option[Array[AnyRef]] = {
-    try {
-      Option(config.getList(path).unwrapped().toArray())
-    } catch {
-      case _: ConfigException.Missing => None
-      case e: ConfigException.WrongType => throw e
-    }
+    listGetter.get(config, path)
+  }
+
+  def getMap(config: Config): Map[String, String] = {
+    import scala.collection.JavaConverters._
+    config.entrySet().asScala.map(x => x.getKey -> x.getValue.unwrapped().toString).toMap
   }
 
   def isDefined(config: Config, path: String): Boolean = {
