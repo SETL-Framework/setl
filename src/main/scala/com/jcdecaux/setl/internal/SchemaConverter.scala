ea431abb3506905d037386e1c0fdbe1e2af56c90
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2020-01-02 18:16:34 +0100

    chore: optimize column rename

--- src/main/scala/com/jcdecaux/setl/internal/SchemaConverter.scala
+++ src/main/scala/com/jcdecaux/setl/internal/SchemaConverter.scala
@@ -77,6 +77,8 @@ object SchemaConverter extends Logging {
           } else {
             false
           }
+
+          // add an empty column only if the DF contains neither the field nor the field's alias
           (!dfContainsFieldName) && (!dfContainsFieldAlias)
       }
 
@@ -149,9 +151,7 @@ object SchemaConverter extends Logging {
       .map(x => x.metadata.getStringArray(ColumnName.toString())(0) -> x.name)
       .toMap
 
-    val newColumns = dataFrame.columns.map(columnName => changes.getOrElse(columnName, columnName))
-
-    dataFrame.toDF(newColumns: _*)
+    dataFrame.transform(renameColumnsOfDataFrame(changes))
   }
 
   /**
@@ -187,9 +187,14 @@ object SchemaConverter extends Logging {
       .map(x => x.name -> x.metadata.getStringArray(ColumnName.toString())(0))
       .toMap
 
-    val newColumns = dataFrame.columns.map(columnName => changes.getOrElse(columnName, columnName))
+    dataFrame.transform(renameColumnsOfDataFrame(changes))
+  }
 
-    dataFrame.toDF(newColumns: _*)
+  private[this] def renameColumnsOfDataFrame(mapping: Map[String, String])
+                                            (dataFrame: DataFrame): DataFrame = {
+    mapping.foldLeft(dataFrame) {
+      (df, change) => df.withColumnRenamed(change._1, change._2)
+    }
   }
 
   /**
@@ -202,7 +207,7 @@ object SchemaConverter extends Logging {
       .map(_.metadata.getStringArray(CompoundKey.toString())(0))
       .toSet
 
-    if (columnsToDrop.nonEmpty && dataFrame.columns.intersect(columnsToDrop.toSeq).isEmpty) {
+    if (columnsToDrop.nonEmpty && dataFrame.columns.intersect(columnsToDrop.toSeq.map(compoundKeyName)).isEmpty) {
       log.warn("Some compound key columns are missing in the data source")
     }
 
