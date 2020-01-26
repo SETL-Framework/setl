05586f106e597f2191a663c05e687edef73de9e9
Xuzhou Qin <me@qinx.dev>
2020-01-26 15:56:13 +0100

    Revert "chore: generate scala doc in test"
    This reverts commit 2fe75d7672faa4ba36932022d6c5f658c50aea06.

--- dev/deploy-snapshot.sh
+++ dev/deploy-snapshot.sh
@@ -5,4 +5,4 @@ set -e
 echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
 echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
 
-mvn clean deploy -ntp -B -DskipTests -Psnapshot -P${SCALA_PROFILE}
+mvn clean deploy scala:doc -ntp -B -DskipTests -Psnapshot -P${SCALA_PROFILE}
