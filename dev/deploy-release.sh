503083f7f03d7a4631d3f420dd168a4d4f6db325
Xuzhou Qin <me@qinx.dev>
2020-01-26 12:54:24 +0100

    chore: migration to travis

---
+++ dev/deploy-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo ${MVN_SETTINGS} > /root/.m2/settings.xml
+echo ${MVN_SECURITY} > /root/.m2/settings-security.xml
+echo ${GPG_KEY} | base64 -d | gpg --import
+
+mvn deploy scala:doc -ntp -B -DskipTests -Prelease -Pwith-source -P${SCALA_PROFILE}
