93a33d5093cb0dea4c82a53beb091dab6a0f2189
Xuzhou Qin <me@qinx.dev>
2020-01-26 15:22:42 +0100

    chore: edit m2 dir path

--- dev/deploy-release.sh
+++ dev/deploy-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo ${MVN_SETTINGS} | base64 -d > /root/.m2/settings.xml
-echo ${MVN_SECURITY} | base64 -d > /root/.m2/settings-security.xml
+echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
+echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
 echo ${GPG_KEY} | base64 -d | gpg --import
 
 mvn clean deploy scala:doc -ntp -B -DskipTests -Prelease -P${SCALA_PROFILE}
--- dev/deploy-snapshot.sh
+++ dev/deploy-snapshot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo ${MVN_SETTINGS} | base64 -d > /root/.m2/settings.xml
-echo ${MVN_SECURITY} | base64 -d > /root/.m2/settings-security.xml
+echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
+echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
 
 mvn clean deploy scala:doc -ntp -B -DskipTests -Psnapshot -P${SCALA_PROFILE}
