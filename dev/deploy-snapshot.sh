d279aeddbd705b281bd9b8726ef6d2f5e000f491
Xuzhou Qin <me@qinx.dev>
2020-01-26 15:07:30 +0100

    fix: clean mvn target before deployment

--- dev/deploy-snapshot.sh
+++ dev/deploy-snapshot.sh
@@ -3,4 +3,4 @@
 echo ${MVN_SETTINGS} | base64 -d > /root/.m2/settings.xml
 echo ${MVN_SECURITY} | base64 -d > /root/.m2/settings-security.xml
 
-mvn deploy scala:doc -ntp -B -DskipTests -Psnapshot -P${SCALA_PROFILE}
+mvn clean deploy scala:doc -ntp -B -DskipTests -Psnapshot -P${SCALA_PROFILE}
