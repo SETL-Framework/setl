927092e80bf7f420236440061cf6cd74a0d3a9eb
Xuzhou Qin <me@qinx.dev>
2020-01-26 16:37:29 +0100

    chore: cross build for scala 2.11 2.12

--- dev/deploy-release.sh
+++ dev/deploy-release.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,4 +6,4 @@ echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
 echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
 echo ${GPG_KEY} | base64 -d | gpg --import
 
-mvn clean deploy scala:doc -ntp -B -DskipTests -Prelease -P${SCALA_PROFILE}
+mvn clean deploy scala:doc -ntp -B -DskipTests -Prelease -Pscala_${SCALA_VER}
--- dev/test.sh
+++ dev/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,4 +6,4 @@ export AWS_ACCESS_KEY_ID="fakeAccess"
 export AWS_SECRET_ACCESS_KEY="fakeSecret"
 export AWS_REGION="eu-west-1"
 
-mvn -B -ntp clean:clean scoverage:report -P${SCALA_PROFILE}
+mvn -B -ntp clean:clean scoverage:report -Pscala_${SCALA_VER}
