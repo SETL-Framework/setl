2fe75d7672faa4ba36932022d6c5f658c50aea06
Xuzhou Qin <me@qinx.dev>
2020-01-26 15:53:36 +0100

    chore: generate scala doc in test

--- dev/test.sh
+++ dev/test.sh
@@ -6,4 +6,4 @@ export AWS_ACCESS_KEY_ID="fakeAccess"
 export AWS_SECRET_ACCESS_KEY="fakeSecret"
 export AWS_REGION="eu-west-1"
 
-mvn -B -ntp clean:clean scoverage:report -P${SCALA_PROFILE}
+mvn -B -ntp clean:clean scoverage:report scala:doc -P${SCALA_PROFILE}
