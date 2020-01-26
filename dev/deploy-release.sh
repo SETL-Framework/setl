04528f8ef010c7683c1838b3465b847b6527a42f
Xuzhou Qin <me@qinx.dev>
2020-01-26 14:50:16 +0100

    fix: encode variables

--- dev/deploy-release.sh
+++ dev/deploy-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo ${MVN_SETTINGS} > /root/.m2/settings.xml
-echo ${MVN_SECURITY} > /root/.m2/settings-security.xml
+echo ${MVN_SETTINGS} | base64 -d > /root/.m2/settings.xml
+echo ${MVN_SECURITY} | base64 -d > /root/.m2/settings-security.xml
 echo ${GPG_KEY} | base64 -d | gpg --import
 
 mvn deploy scala:doc -ntp -B -DskipTests -Prelease -P${SCALA_PROFILE}
