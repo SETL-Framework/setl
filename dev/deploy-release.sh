ee0d37f008e3e6d651b84c491e2f0d709baf54ad
Xuzhou Qin <xuzhou.qin@jcdecaux.com>
2020-02-14 16:57:48 +0100

    fix: cross build
    Signed-off-by: Xuzhou Qin <xuzhou.qin@jcdecaux.com>

--- dev/deploy-release.sh
+++ dev/deploy-release.sh
@@ -6,4 +6,4 @@ echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
 echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
 echo ${GPG_KEY} | base64 -d | gpg --import
 
-mvn clean deploy scala:doc -ntp -B -DskipTests -Prelease -Pscala_${SCALA_VER} -Pspark_${SPARK_VER}
+mvn clean deploy scala:doc -ntp -B -DskipTests -P release,scala_${SCALA_VER},spark_${SPARK_VER}
