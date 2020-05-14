1ab67c04e775947d63027bddcfa41fed2938e3dd
Xuzhou Qin <me@qinx.dev>
2020-05-14 16:02:04 +0200

    feat: add StructuredStreamingConnector

---
+++ src/main/java/com/jcdecaux/setl/annotation/Experimental.java
@@ -0,0 +1,15 @@
+package com.jcdecaux.setl.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The Experimental annotation indicate that the annotated class/method/field is supposed to be an experimental feature,
+ * thus the stability can't be guaranteed.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
+public @interface Experimental {
+}
--- src/main/scala/com/jcdecaux/setl/internal/CanCreate.scala
+++ src/main/scala/com/jcdecaux/setl/internal/CanCreate.scala
@@ -4,8 +4,19 @@ import org.apache.spark.sql.DataFrame
 
 trait CanCreate {
 
+  /**
+   * Create a data storage (e.g. table in a database or file/folder in a file system) with a suffix
+   *
+   * @param t      data frame to be written
+   * @param suffix suffix to be appended at the end of the data storage name
+   */
   def create(t: DataFrame, suffix: Option[String]): Unit
 
+  /**
+   * Create a data storage (e.g. table in a database or file/folder in a file system)
+   *
+   * @param t data frame to be written
+   */
   def create(t: DataFrame): Unit
 
 }
