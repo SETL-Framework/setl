3aaddde38fc1a510a978df3fbfb6fc844acae4e9
XuzhouQin <17144939+qxzzxq@users.noreply.github.com>
2020-05-26 22:09:20 +0200

    Update deploy-release.sh

--- dev/deploy-release.sh
+++ dev/deploy-release.sh
@@ -4,6 +4,6 @@ set -e
 
 echo ${MVN_SETTINGS} | base64 -d > ${HOME}/.m2/settings.xml
 echo ${MVN_SECURITY} | base64 -d > ${HOME}/.m2/settings-security.xml
-echo ${GPG_KEY} | base64 -d | gpg --import
+echo ${GPG_KEY} | base64 -d | gpg --import --batch > /dev/null 2>&1
 
 mvn clean deploy scala:doc -ntp -B -DskipTests -P release,spark_${SPARK_VER}
