bb438987a7fef16375398013cc13c65242875225
Xuzhou Qin <me@qinxuzhou.com>
2020-08-25 11:31:57 +0200

    doc(Connector): add custome connector docs

--- src/test/scala/com/jcdecaux/setl/TestObject.scala
+++ src/test/scala/com/jcdecaux/setl/TestObject.scala
@@ -3,6 +3,7 @@ package com.jcdecaux.setl
 import java.sql.{Date, Timestamp}
 
 import com.jcdecaux.setl.config.Conf
+import com.jcdecaux.setl.internal.CanDrop
 import com.jcdecaux.setl.storage.connector.ConnectorInterface
 import com.typesafe.config.Config
 import org.apache.spark.sql.DataFrame
@@ -14,7 +15,7 @@ case class TestObject3(partition1: Int, partition2: String, clustering1: String,
 
 case class TestObject2(col1: String, col2: Int, col3: Double, col4: Timestamp, col5: Date, col6: Long)
 
-class CustomConnector extends ConnectorInterface {
+class CustomConnector extends ConnectorInterface with CanDrop {
   override def setConf(conf: Conf): Unit = null
 
   override def setConfig(config: Config): Unit = null
@@ -27,4 +28,9 @@ class CustomConnector extends ConnectorInterface {
   override def write(t: DataFrame, suffix: Option[String]): Unit = log.debug("Write with suffix")
 
   override def write(t: DataFrame): Unit = log.debug("Write")
+
+  /**
+   * Drop the entire table.
+   */
+  override def drop(): Unit = log.debug("drop")
 }
